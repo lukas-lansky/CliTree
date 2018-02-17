67034eb3ba60e3d32582b61db05469f6de136c27
Lukáš Lánský <lukas@lansky.name>
2018-02-17 15:32:59 +0100

    Line drawing

--- Lansky.CliTree.Test/CliTreeTest.cs
+++ Lansky.CliTree.Test/CliTreeTest.cs
@@ -1,7 +1,6 @@
-using Lansky.CliTree.ConsoleWrapper;
+﻿using Lansky.CliTree.ConsoleWrapper;
 using Lansky.CliTree.TreeRepresentation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 namespace Lansky.CliTree.Test
 {
@@ -26,7 +25,84 @@ namespace Lansky.CliTree.Test
 
             Assert.AreEqual(2, console.GetOutputLines().Count);
             Assert.AreEqual("root", console.GetOutputLines()[0]);
-            Assert.AreEqual("  child", console.GetOutputLines()[1]);
+            Assert.AreEqual("└─ child", console.GetOutputLines()[1]);
+        }
+
+        [TestMethod]
+        public void ManyChildrenOfRootNode()
+        {
+            var console = new StringBuilderConsole();
+            CliTree.Print(
+                new AdHocTree("root",
+                    new AdHocTree("child 1"),
+                    new AdHocTree("child 2"),
+                    new AdHocTree("child 3")),
+                console);
+
+            Assert.AreEqual(4, console.GetOutputLines().Count);
+            Assert.AreEqual("root", console.GetOutputLines()[0]);
+            Assert.AreEqual("├─ child 1", console.GetOutputLines()[1]);
+            Assert.AreEqual("├─ child 2", console.GetOutputLines()[2]);
+            Assert.AreEqual("└─ child 3", console.GetOutputLines()[3]);
+        }
+
+        [TestMethod]
+        public void DeepChildrenOfRootNode()
+        {
+            var console = new StringBuilderConsole();
+            CliTree.Print(
+                new AdHocTree("root",
+                    new AdHocTree("child",
+                        new AdHocTree("grandchild",
+                            new AdHocTree("grandgrandchild")))), console);
+
+            Assert.AreEqual(4, console.GetOutputLines().Count);
+            Assert.AreEqual("root", console.GetOutputLines()[0]);
+            Assert.AreEqual("└─ child", console.GetOutputLines()[1]);
+            Assert.AreEqual("  └─ grandchild", console.GetOutputLines()[2]);
+            Assert.AreEqual("    └─ grandgrandchild", console.GetOutputLines()[3]);
+        }
+
+        [TestMethod]
+        public void DeepChildrenOfRootNodeWithOneMoreChildlessChild()
+        {
+            var console = new StringBuilderConsole();
+            CliTree.Print(
+                new AdHocTree("root",
+                    new AdHocTree("child 1",
+                        new AdHocTree("grandchild",
+                            new AdHocTree("grandgrandchild"))),
+                    new AdHocTree("child 2")), console);
+
+            Assert.AreEqual(5, console.GetOutputLines().Count);
+            Assert.AreEqual("root", console.GetOutputLines()[0]);
+            Assert.AreEqual("├─ child 1", console.GetOutputLines()[1]);
+            Assert.AreEqual("│ └─ grandchild", console.GetOutputLines()[2]);
+            Assert.AreEqual("│   └─ grandgrandchild", console.GetOutputLines()[3]);
+            Assert.AreEqual("└─ child 2", console.GetOutputLines()[4]);
+        }
+
+        [TestMethod]
+        public void JustAnotherDeepChildrenSituation()
+        {
+            var console = new StringBuilderConsole();
+            CliTree.Print(
+                new AdHocTree("root",
+                    new AdHocTree("child 1",
+                        new AdHocTree("grandchild 1",
+                            new AdHocTree("grandgrandchild 1"),
+                            new AdHocTree("grandgrandchild 2")),
+                        new AdHocTree("grandchild 2")),
+                    new AdHocTree("child 2")), console);
+
+            Assert.AreEqual(7, console.GetOutputLines().Count);
+            Assert.AreEqual("root", console.GetOutputLines()[0]);
+            Assert.AreEqual("├─ child 1", console.GetOutputLines()[1]);
+            Assert.AreEqual("│ ├─ grandchild 1", console.GetOutputLines()[2]);
+            Assert.AreEqual("│ │ ├─ grandgrandchild 1", console.GetOutputLines()[3]);
+            Assert.AreEqual("│ │ └─ grandgrandchild 2", console.GetOutputLines()[4]);
+            Assert.AreEqual("│ └─ grandchild 2", console.GetOutputLines()[5]);
+            Assert.AreEqual("└─ child 2", console.GetOutputLines()[6]);
         }
     }
 }
--- Lansky.CliTree/CliTree.cs
+++ Lansky.CliTree/CliTree.cs
@@ -1,6 +1,8 @@
 ﻿using Lansky.CliTree.ConsoleWrapper;
 using Lansky.CliTree.TreeRepresentation;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Lansky.CliTree
 {
@@ -15,18 +17,55 @@ namespace Lansky.CliTree
         {
             config = config ?? new DisplayConfiguration();
 
-            var stack = new Stack<(ITree tree, int depth)>();
-            stack.Push((tree, 0));
+            var stack = new Stack<(ITree tree, int depth, List<bool> last)>();
+            stack.Push((tree, 0, new List<bool>()));
 
             while (stack.Count > 0)
             {
-                (var t, var d) = stack.Pop();
+                (var t, var d, var last) = stack.Pop();
 
-                console.WriteLine(new string(' ', d * 2) + t.RootValue);
-
-                foreach (var subtree in t.SubTrees)
+                var line = new StringBuilder();
+                for (var i = 0; i < d; i++)
                 {
-                    stack.Push((subtree, d + 1));
+                    if (i != d - 1)
+                    {
+                        if (!last[i])
+                        {
+                            line.Append('│');
+                        }
+                        else
+                        {
+                            line.Append(' ');
+                        }
+                    }
+                    else
+                    {
+                        if (!last[i])
+                        {
+                            line.Append('├');
+                        }
+                        else
+                        {
+                            line.Append('└');
+                        }
+                    }
+
+                    if (i == d - 1)
+                    {
+                        line.Append("─");
+                    }
+
+                    line.Append(' ');
+                }
+                line.Append(t.RootValue);
+                console.WriteLine(line.ToString());
+
+                var st = t.SubTrees.ToList();
+                for (var i = st.Count - 1; i >= 0; i--)
+                {
+                    var newLast = new List<bool>(last); // TODO, this is quite wasteful
+                    newLast.Add(i == st.Count - 1);
+                    stack.Push((st[i], d + 1, newLast));
                 }
             }
         }
